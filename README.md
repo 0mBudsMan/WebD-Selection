b8d53268ea3c6dd8641742b43f407438014fa3d0
Om Buddhadev <128999558+0mBudsMan@users.noreply.github.com>
2023-08-07 15:44:58 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,74 @@
-# WebD-Selection
+
+
+## Introduction
+
+The Selection Process would involve 3 phases. The students shortlisted in phase 1 **would be split into buckets according to their skillset or interest (frontend or backend) and then given a task for their specific bucket. This task will not be a basic task like Round 1, we would expect you to show your expertise in your skillset, even aspects like documentation will be considered.**
+
+The third phase would be interviews, the details of which would be shared later.
+
+## Phase 1: Deadline - 8/7/23 11:59pm
+
+### Starters
+
+1. Create a private repository named `WebD-Selection-Task-1` on GitHub.
+2. Write your Name, Enrollment Number, WhatsApp number, and preferred bucket (Frontend/Backend) in the README.md file of your repo.
+3. Add the following GitHub profiles as collaborators to the repository:
+   - atharva-gadekar
+   - BuddyLongLegs
+   - GammaMicrowave
+   - Manas2403
+4. Clone the `WebD-Selection-Task-1` repository to your local system.
+5. Fill [this](https://forms.gle/DwunA4dYkZuWDwQL8) Google Form for becoming a part of selection process.
+6. Add the provided boilerplate code files to your codebase (Optional).
+
+### Main Course - DSA GeekSheet
+
+All tasks here are compulsory.
+
+1. Fetch the questions along with their category titles from the API Link given below.
+2. Display the questions in an accordion format.
+3. Use the category title as the accordion heading, and the questions should expand on clicking the title.
+4. Ensure your website is responsive for a larger dataset. We will change the amount of questions and topics in the API during testing.
+
+### Dessert (Optional)
+
+You will earn Bonus Points if you complete any or all of the following tasks:
+
+1. Add a checkbox before each question. When a question is marked, change its background color to green. (10 points)
+2. Implement a dark mode toggle button to enhance the user experience. (20 points)
+3. Add keyboard shortcuts to navigate through questions easily (e.g., arrow keys). (20 points)
+4. Include a search bar that allows users to search for specific questions by keywords or category titles. (20 points)
+5. Store the marked questions so that the information is preserved even after the website is closed/reloaded. (30 points)
+6. Create a progress bar that tracks the user's completion rate of answered questions. (30 points)
+7. Add a feature that allows users to bookmark questions for later review. The bookmarked questions must be displayed together on a new page/section (40 points)
+8. Allow users to create a new category and add a question under any category. Ensure this information is stored in the browser. (70 points)
+
+API Link: 
+```
+https://test-data-gules.vercel.app/data.json
+```
+
+
+## Rules
+
+1. Deploy your website on GitHub Pages (it is free for students) or any other platform.
+2. Use only vanilla CSS and JavaScript; external frameworks like Bootstrap are not allowed. Also creating a backend or using another API is not allowed.
+3. Your code will be checked for plagiarism, so avoid copying code from others. In cases where significant similarities or direct copying are found, the respective submission will be disqualified from the selection process.
+4. Avoid copying directly from ChatGPT or any other AI. We expect original work from all candidates.
+5. You are encouraged to seek guidance and learn from various web development resources, but ensure that your final submission represents your unique work and creativity.
+
+## Scoring
+
+- Starters: 50 points
+- Main Course - DSA GeekSheet: 200 points
+- Dessert (Optional Tasks): Up to 240 bonus points
+- Code Quality, Responsiveness: Up to 200 points
+
+## Testing
+
+We will test your website using a larger dataset to ensure responsiveness.
+
+## Support
+For any doubts/queries you can add a comment in this [issue](https://github.com/GeekHaven/WebD-Selection-23-24-Task1/issues/1).
+
+00110101 01000001 01001110
---
+++ script.js
@@ -0,0 +1,244 @@
+// Code Here
+// API Link: -https://test-data-gules.vercel.app/data.json
+
+//dark and light hteme
+var themeicon = document.getElementsByClassName('sunicon')[0];
+themeicon.onclick = function(){
+   document.body.classList.toggle('light');
+   if(themeicon.classList.contains('fa-sun')){
+      themeicon.classList.remove('fa-sun');
+      themeicon.classList.remove('fa-regular');
+      themeicon.classList.add('fa-solid');
+      themeicon.classList.add('fa-moon');
+   }
+   else{
+      themeicon.classList.add('fa-sun');
+      themeicon.classList.add('fa-regular');
+      themeicon.classList.remove('fa-solid');
+      themeicon.classList.remove('fa-moon');
+   }
+}
+
+
+var savedquestion = []; var noofquestionsdone=0; var total=0;
+
+
+
+async function fetchQuestionDetailsJSON() {
+  const response = await fetch('https://test-data-gules.vercel.app/data.json');
+  const wholedata = await response.json();
+  const {data} = wholedata;
+  return data;
+}
+
+fetchQuestionDetailsJSON().then(listofques => {
+    var temp=1;
+ listofques.forEach(user => {
+    const e1 = document.createElement('div');
+    e1.classList.add('c');
+    const tempp='faq-' + temp;
+    const inp=document.createElement('input');
+    inp.setAttribute('type', 'checkbox');
+    inp.setAttribute('id', tempp);
+    inp.classList.add('onlyforthis')
+    e1.appendChild(inp);
+
+    const hone = document.createElement('h1');
+    const label = document.createElement('label');
+    label.classList.add('onlyforhis')
+    
+   
+    label.setAttribute('for', tempp);
+    const text = document.createTextNode(user.title);
+    label.appendChild(text);
+    hone.appendChild(label);
+    e1.appendChild(hone);
+
+    const deev = document.createElement('div');
+    deev.classList.add('p');
+
+  
+    
+    
+    
+    const content = document.createElement('p');
+    
+    const leest=document.createElement('div'); leest.classList.add('leest');
+    content.appendChild(leest);
+
+    const centerbring = document.createElement('div'); centerbring.classList.add('centerbring');
+    leest.appendChild(centerbring);
+
+    const stronger = document.createElement('strong'); stronger.classList.add('stronger');
+    const titlee =document.createTextNode("List of questions with links");
+   stronger.appendChild(titlee);
+
+   centerbring.appendChild(stronger);
+
+   deev.appendChild(content);
+   e1.appendChild(deev);
+
+
+   const ol = document.createElement('ol'); 
+   var temp2=1;
+   total+=user.ques.length;
+   
+   //console.log(total);
+   user.ques.forEach((questions)=>{
+     
+
+      
+      const {id, p1_link, p2_link, tags,title,yt_link} = questions;
+     
+      const li = document.createElement('li');
+      const strongg = document.createElement('strong'); strongg.classList.add('strongg'); strongg.classList.add('key-'+temp2);
+      strongg.appendChild(document.createTextNode(title));
+      const inpu = document.createElement('input');
+      inpu.classList.add('forprogressbar')
+      inpu.setAttribute('type', 'checkbox'); inpu.setAttribute('id', 'faq-'+temp+'faaq-' + temp2); 
+      inpu.setAttribute('onclick', 'progress_bar()');
+      const labe = document.createElement('label'); labe.setAttribute('for', 'faq-'+temp+'faaq-' + temp2);
+      li.appendChild(inpu);
+      li.appendChild(labe);
+      labe.appendChild(strongg);
+
+
+      const para = document.createElement('p');
+      if(tags!=0){
+      para.appendChild(document.createTextNode("Tags: " + tags));
+   para.appendChild(document.createElement('hr'))}
+      if(yt_link !=0){
+         const ytlogo = document.createElement('i');
+         ytlogo.classList.add('fa-brands'); ytlogo.classList.add('fa-youtube');  ytlogo.classList.add('fa-xl');
+         const anchor = document.createElement('a');
+         anchor.setAttribute('href', yt_link);
+         anchor.appendChild(ytlogo);
+         para.appendChild(document.createTextNode("YouTube: "));
+         para.appendChild(anchor)
+         para.appendChild(document.createElement('hr'))
+
+        
+         //para.appendChild(document.createTextNode("YouTube: " + yt_link));  para.appendChild(ytlogo)
+      }
+      if(p1_link !=null){
+         const p1logo = document.createElement('i');
+         p1logo.classList.add('fa-solid'); p1logo.classList.add('fa-terminal');  p1logo.classList.add('fa-xl');
+         const anchor = document.createElement('a');
+         anchor.setAttribute('href', p1_link);
+         anchor.appendChild(p1logo);
+         para.appendChild(document.createTextNode("Platform Link: "));
+         para.appendChild(anchor)
+         para.appendChild(document.createElement('hr'))
+
+        
+         //para.appendChild(document.createTextNode("YouTube: " + yt_link));  para.appendChild(ytlogo)
+      }
+      if(p2_link !=null){
+         const p2logo = document.createElement('i');
+         p2logo.classList.add('fa-solid'); p2logo.classList.add('fa-terminal');  p2logo.classList.add('fa-xl');
+         const anchor = document.createElement('a');
+         anchor.setAttribute('href', p2_link);
+         anchor.appendChild(p2logo);
+         para.appendChild(document.createTextNode("Platform Link: "));
+         para.appendChild(anchor)
+         para.appendChild(document.createElement('hr'))
+
+        
+         //para.appendChild(document.createTextNode("YouTube: " + yt_link));  para.appendChild(ytlogo)
+      }
+     
+
+      labe.appendChild(para);
+      li.appendChild(labe);
+      
+   
+
+      
+      ol.appendChild(li);
+      temp2++;
+   });
+   leest.appendChild(ol);
+   
+  
+
+    
+    temp++;
+    
+    document.getElementsByClassName('accordion')[0].appendChild(e1)
+   
+ })
+ const progbar = document.createElement('h1');
+   progbar.classList.add('toggle');
+   
+   const strifng = "Number of questions attempted: " + noofquestionsdone + " out of " + total;
+   progbar.appendChild(document.createTextNode(strifng));
+   document.getElementById('progressbar').appendChild(progbar);
+
+});
+
+function search_question() {
+   let input = document.getElementById('searchbar').value
+   input=input.toLowerCase();
+   let x = document.getElementsByClassName('strongg');
+     
+   for (i = 0; i < x.length; i++) { 
+       if (!x[i].innerHTML.toLowerCase().includes(input) || input==="") {
+           x[i].style.background='none';
+       }
+       else {
+         x[i].style.background='yellow';                 
+       }
+   }
+}
+
+
+
+function progress_bar(){
+   noofquestionsdone=0;
+   var inpt = document.getElementsByClassName('forprogressbar');
+  for(var i=0;i<inpt.length;i++){
+   if(inpt[i].checked) {noofquestionsdone++;}}
+      document.getElementsByClassName('toggle')[0].remove();
+      const progbar = document.createElement('h1');
+progbar.classList.add('toggle');
+const percentage = noofquestionsdone/total * 100;
+const strifng = "Number of questions attempted: " + noofquestionsdone + " out of " + total + " ("+percentage+"% )";
+progbar.appendChild(document.createTextNode(strifng));
+document.getElementById('progressbar').appendChild(progbar);
+
+   }
+  
+var whichques=1;
+
+document.addEventListener('keyup', (e)=>{
+   if(e.keyCode===78){
+      const allques = document.getElementsByClassName('strongg');
+      for(var i=0;i<allques.length;i++){
+         allques[i].style.background='';
+      }
+      const classofques="key-"+whichques;
+      const questions = document.getElementsByClassName(classofques);
+      for(var i=0;i<questions.length;i++){
+         questions[i].style.background='orange'
+      }
+      whichques++;
+   }
+   else if(e.keyCode===80){
+      console.log('p pressed');
+      whichques--;
+      if(whichques>=1){
+    const allques = document.getElementsByClassName('strongg');
+      for(var i=0;i<allques.length;i++){
+         allques[i].style.background='';
+      }
+      const classofques="key-"+whichques;
+      const questions = document.getElementsByClassName(classofques);
+      for(var i=0;i<questions.length;i++){
+         questions[i].style.background='orange'
+      }
+   }
+   }
+   
+})
+
+
